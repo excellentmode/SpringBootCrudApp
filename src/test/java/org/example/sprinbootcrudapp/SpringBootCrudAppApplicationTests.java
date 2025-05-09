decc6c1f44a5a97ec91618f94a70a6200ff02c39
excellentmode <mynotionbook13@gmail.com>
2025-05-09 13:20:12 +0700

    init commit

---
+++ src/test/java/org/example/sprinbootcrudapp/SpringBootCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sprinbootcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
